a6f47a5ebbd42a2bb65f3a9ce58bfefdece5e09d
Saud Zahir <m.saud.zahir@gmail.com>
2024-06-19 13:08:40 +0500

    Add Recursion

---
+++ Recursion/exponential.c
@@ -0,0 +1,41 @@
+/*
+ * Author  : Saud Zahir
+ * Date    : June 19, 2024
+ * Contact : m.saud.zahir@gmail.com
+*/
+
+
+#include "stdio.h"
+
+
+int factorial(int n) {
+    if (n == 0 || n == 1) {
+        return 1;
+    }
+    else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int n = 10;
+    float exp = 0.0;
+    float x;
+
+    printf("**************************\n");
+    printf("** Exponential Function **\n");
+    printf("**************************\n");
+    printf("x = ");
+    scanf("%f", &x);
+
+    for (int i = 0; i <= n; i++) {
+        float pow = 1.0;
+        for (int k = 1; k <= i; k++) {
+            pow *= x;
+        }
+        exp += pow/factorial(i);
+    }
+    printf("exp(%d) = %.32f\n", x, exp);
+
+    return 0;
+}
