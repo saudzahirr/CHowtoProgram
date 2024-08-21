36c68dee6af63b877b32cd595d3f4ff5bbd41397
saudz <saud.zahir@soco-engineers.com>
2024-08-21 15:20:00 +0500

    Add static arrays and bubble sort

---
+++ chapter-06/bubble_sort.c
@@ -0,0 +1,39 @@
+/*
+ * Author  : Saud Zahir
+ * Date    : August 21, 2024
+ * Contact : m.saud.zahir@gmail.com
+ * 
+ * 
+ * Topic: Bubble Sort
+*/
+
+#include <stdio.h>
+#define SIZE 10
+
+int main(void) {
+    int a[SIZE] = {2, 29, 14, 8, 12, 10, 89, 68, 45, 37};
+
+    puts("Data items in original order");
+
+    for (size_t i = 0; i < SIZE; i++) {
+        printf("%4d", a[i]);
+    }
+
+    // bubble sort
+    for (unsigned int pass = 1; pass < SIZE; pass++) {
+        for (size_t i = 0; i < SIZE - 1; i++) {
+            if (a[i] > a[i + 1]) {
+                int hold = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = hold;
+            }
+        }
+    }
+    puts("\nData items in ascending order");
+
+    for (size_t i = 0; i < SIZE; i++)
+    {
+        printf("%4d", a[i]);
+    }
+    puts("");
+}
---
+++ chapter-06/static_arrays.c
@@ -0,0 +1,62 @@
+/*
+ * Author  : Saud Zahir
+ * Date    : August 21, 2024
+ * Contact : m.saud.zahir@gmail.com
+ * 
+ * 
+ * Topic: Static Arrays
+*/
+
+#include <stdio.h>
+#define SIZE 8
+
+void staticArray(void);
+
+void autoArray(void);
+
+int main() {
+    autoArray();
+    staticArray();
+    puts("");
+    autoArray();
+    // The second time the function is called, the static array contains
+    // the values stored during the first function call.
+    staticArray();
+
+    return 0;
+}
+
+void staticArray(void) {
+    static int array[SIZE];
+    ++array[0];
+    ++array[2];
+    ++array[4];
+    
+    for (size_t i = 0; i < SIZE; i++) {
+        printf("%d ", array[i]);
+    }
+    puts("");
+}
+
+void autoArray(void) {
+    auto int array[SIZE] = {0}; // Same as int array[SIZE] = {0}
+    ++array[0];
+    ++array[2];
+    ++array[4];
+    
+    for (size_t i = 0; i < SIZE; i++) {
+        printf("%d ", array[i]);
+    }
+    puts("");
+}
+
+/*
+NOTE:
+    A static local array is not created and initialized each time the function
+    is called and the array is not destroyed each time the function is exited
+    in the program. This reduces program execution time, particularly for programs
+    with frequently called functions that contain large arrays.
+
+    Arrays that are static are initialized once at program startup. If not explicitly
+    initialize a static array, that array’s elements are initialized to zero by default.
+*/
