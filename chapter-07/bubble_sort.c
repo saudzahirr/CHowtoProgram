8d436e98ae0e07c94b23afb175e3732be569eb1d
saudz <saud.zahir@soco-engineers.com>
2024-10-09 17:26:44 +0500

    Add sizeof operator

---
+++ chapter-07/bubble_sort.c
@@ -0,0 +1,50 @@
+/*
+ * Author  : Saud Zahir
+ * Date    : October 09, 2024
+ * Contact : m.saud.zahir@gmail.com
+ * 
+ * 
+ * Topic: Bubble Sort
+*/
+
+#include <stdio.h>
+#define SIZE 10
+
+void bubbleSort(int * const, const size_t);
+
+int main(void) {
+    int a[SIZE] = {2, 29, 14, 8, 12, 10, 89, 68, 45, 37};
+
+    puts("Data items in original order");
+
+    for (size_t i = 0; i < SIZE; i++) {
+        printf("%4d", a[i]);
+    }
+
+    bubbleSort(a, SIZE);
+    puts("\nData items in ascending order");
+
+    for (size_t i = 0; i < SIZE; i++)
+    {
+        printf("%4d", a[i]);
+    }
+    puts("");
+}
+
+void bubbleSort(int * const array, const size_t size) {
+    void swap(int *, int *);
+    // bubble sort
+    for (unsigned int pass = 1; pass < SIZE; pass++) {
+        for (size_t i = 0; i < SIZE - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                swap(&array[i], &array[i + 1]);
+            }
+        }
+    }
+}
+
+void swap(int *aPtr, int *bPtr) {
+    int temp = *aPtr;
+    *aPtr = *bPtr;
+    *bPtr = temp;
+}
---
+++ chapter-07/operator_sizeof.c
@@ -0,0 +1,50 @@
+/*
+ * Author  : Saud Zahir
+ * Date    : October 09, 2024
+ * Contact : m.saud.zahir@gmail.com
+ * 
+ * 
+ * Topic: Operator sizeof
+*/
+
+#include <stdio.h>
+
+int main(void) {
+    char c;
+    short s;
+    int i;
+    long l;
+    long long ll;
+    float f;
+    double d;
+    long double ld;
+    int array[20];    // create array of 20 int elements
+    int *ptr = array; // create pointer to array
+
+    printf("sizeof c = %lu\tsizeof(char) = %lu\n", sizeof(c), sizeof(char));
+    printf("sizeof s = %lu\tsizeof(short) = %lu\n", sizeof(s), sizeof(short));
+    printf("sizeof i = %lu\tsizeof(int) = %lu\n", sizeof(i), sizeof(int));
+    printf("sizeof l = %lu\tsizeof(long) = %lu\n", sizeof(l), sizeof(long));
+    printf("sizeof ll = %lu\tsizeof(long) = %lu\n", sizeof(ll), sizeof(long long));
+    printf("sizeof f = %lu\tsizeof(float) = %lu\n", sizeof(f), sizeof(float));
+    printf("sizeof d = %lu\tsizeof(double) = %lu\n", sizeof(d), sizeof(double));
+    printf("sizeof ld = %lu\tsizeof(long double) = %lu\n", sizeof(ld), sizeof(long double));
+    printf("sizeof int array = %lu\n", sizeof array);
+    printf("sizeof int *ptr = %lu\n", sizeof ptr);
+
+    return 0;
+}
+
+/*
+NOTE:
+    C provides the special unary operator sizeof to determine
+    the size in bytes of an array (or any other data type). This
+    operator is applied at compilation time, unless its operand
+    is a variable-length array.
+
+    The size of ptr will be 4 bytes on 32-bit systems and 8 bytes
+    on 64-bit systems.
+ 
+    $ getconf LONG_BIT
+    >64
+*/
