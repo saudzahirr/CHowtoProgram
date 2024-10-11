77e958490d7a141f445210bed019c2f491941ef6
saudz <saudz@gmail.com>
2024-10-11 07:43:19 +0500

    Update

---
+++ chapter-02/start.c
@@ -0,0 +1,32 @@
+/*
+ * Author  : Saud Zahir
+ * Date    : October 11, 2024
+ * Contact : m.saud.zahir@gmail.com
+ * 
+ * 
+ * Topic: A Simple C Program
+*/
+
+#include <stdio.h>
+
+// Main function begins program execution
+int main( void ) {
+    printf("Hello, world!");
+    return 0;
+}
+
+/*
+NOTE:
+    // indicate single line comments.
+    #include is a C preprocessor. Lines beginning
+    with # are processed by the preprocessor before
+    compilation.
+    Every program in C begins execution at the function
+    main.
+    The pair of braces and the portion of the program
+    between the braces is called a block.
+    The line of code ending with semicolon (;) is called
+    a statement and semicolon is statement terminator.
+*/
+
+// '/*...*/' indicate the start of multiline comments
